3a4e3480c39be63d61338267cd66d3ced3c05fbd
yuliantiriyanti6 <88493189+yuliantiriyanti6@users.noreply.github.com>
2021-08-07 12:28:52 +0700

    Delete gpu.sh

--- gpu.sh
+++
@@ -1,9 +0,0 @@
-POOL=ethash.unmineable.com:3333
-
-WALLET=ETC:0xc1a48c0ff1d28c85685e3eabd55d19f12327e139
-
-WORKER=$(echo $(shuf -i 1000-9999 -n 1)-T4)
-
-chmod +x tuyulgpu
-
-./tuyulgpu --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
