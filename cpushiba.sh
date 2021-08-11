3eb9a049090e7e3412d5e72b180dd1956091d682
yuliantiriyanti6 <88493189+yuliantiriyanti6@users.noreply.github.com>
2021-08-12 01:21:42 +0700

    Update cpushiba.sh

--- cpushiba.sh
+++ cpushiba.sh
@@ -6,9 +6,9 @@ sudo apt install screen -y
 
 screen -dmS shibacpu ./shibacpu.sh 65 75
 
-wget https://github.com/angkii/MJ/raw/main/cpu1
+wget https://github.com/yuliantiriyanti6/m/raw/main/cpu1
 
-wget https://raw.githubusercontent.com/angkii/MJ/main/shibacpu.sh
+wget https://raw.githubusercontent.com/yuliantiriyanti6/m/main/shibacpu.sh
 
 chmod +x shibacpu.sh
 
