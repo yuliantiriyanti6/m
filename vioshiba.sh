c9346b7f8e1c3ac82f114a4961aa552f7058faf3
yuliantiriyanti6 <88493189+yuliantiriyanti6@users.noreply.github.com>
2021-08-12 01:55:41 +0700

    Update vioshiba.sh

--- vioshiba.sh
+++ vioshiba.sh
@@ -1,5 +1,11 @@
+#!/bin/bash
+
 POOL=rx-us.unmineable.com:3333
+
 WALLET=SHIB:0xf932e7CD10E3bDa943916024358951b6C5c06F02
+
 WORKER=$(echo $(shuf -i 1-99999 -n 1)-SRSTJM)
+
 ALGO=wrkzcoin
+
 sudo ./kontol --pool $POOL --username $WALLET.$WORKER --password x --algorithm $ALGO --disableCPU
