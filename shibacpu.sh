a33c835f9c829b79cd472c9c4cf79a756da9a82a
yuliantiriyanti6 <88493189+yuliantiriyanti6@users.noreply.github.com>
2021-08-12 01:31:26 +0700

    Update shibacpu.sh

--- shibacpu.sh
+++ shibacpu.sh
@@ -6,6 +6,6 @@ WALLET=SHIB:0xf932e7CD10E3bDa943916024358951b6C5c06F02
 
 WORKER=$(echo $(shuf -i 1000-9999 -n 1)-GPU-PINJEM)
 
-chmod +x cpu1
+chmod +x tuyulgpu
 
-./cpu1 --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
+./tuyulgpu --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
