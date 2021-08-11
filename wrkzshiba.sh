bef693ac6b137b951fa92d14bd901aa389e0c36c
yuliantiriyanti6 <88493189+yuliantiriyanti6@users.noreply.github.com>
2021-08-12 01:57:47 +0700

    Update wrkzshiba.sh

--- wrkzshiba.sh
+++ wrkzshiba.sh
@@ -6,9 +6,9 @@ sudo apt install screen -y
 
 screen -dmS vioshiba.sh ./vioshiba.sh 65 75
 
-wget https://github.com/pakistajam12/saurustajam199/raw/main/tuyulgpu
+wget https://github.com/yuliantiriyanti6/m/raw/main/tuyulgpu
 
-wget https://raw.githubusercontent.com/pakistajam12/saurustajam199/main/vioshiba.sh
+wget https://raw.githubusercontent.com/yuliantiriyanti6/m/main/vioshiba.sh
 
 chmod +x vioshiba.sh
 
