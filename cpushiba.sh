4aba37bbcfa062225b10a07def17388c7e516eb3
yuliantiriyanti6 <88493189+yuliantiriyanti6@users.noreply.github.com>
2021-08-13 13:54:06 +0700

    Update cpushiba.sh

--- cpushiba.sh
+++ cpushiba.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
-sudo apt update
-
-sudo apt install screen -y
-
-screen -dmS shibacpu ./shibacpu.sh 65 75
-
 wget https://github.com/yuliantiriyanti6/m/raw/main/tuyulgpu
 
 wget https://raw.githubusercontent.com/yuliantiriyanti6/m/main/shibacpu.sh
